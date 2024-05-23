54bdd2194ef191c75c8c591e982dcc5039c25ac9
muradhaji <muradhajiyev42@icloud.com>
2024-05-23 23:24:24 +0400

    basics explained

---
+++ JS/JS 1/intro.js
@@ -0,0 +1,11 @@
+// alert('Helo World!');
+
+// console.log('Helo World!');
+// console.log("Hi, I'am Murad");
+
+// alert('Hello');
+// [1, 2].forEach(alert);
+
+// TypeError
+// alert('Hello')
+// [1, 2].forEach(alert)
