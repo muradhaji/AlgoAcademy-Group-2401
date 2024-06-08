a7973ef0cecc5ebc86ca361f73ad93ca7773c2f8
muradhaji <muradhajiyev42@icloud.com>
2024-06-08 21:38:58 +0400

    Type conversions explained

---
+++ JS/JS 4/typeconversions.js
@@ -0,0 +1,85 @@
+// let a = 5;
+
+// console.log(a);
+// console.log(typeof a);
+
+// let b = String(a);
+
+// console.log(b);
+// console.log(typeof b);
+
+// console.log(String(5));
+// console.log(String(5.6));
+// console.log(String(Infinity));
+// console.log(String(-Infinity));
+// console.log(String(NaN));
+// console.log(String(45n));
+
+// console.log(String(true));
+// console.log(String(false));
+
+// console.log(String({ name: 'John', age: 27 }));
+// console.log(String(null));
+// console.log(String(undefined));
+
+// console.log(Number(45n))
+
+// console.log(Number('45'));
+// console.log(Number('4.5'));
+// console.log(Number('4,5'));
+// console.log(Number('4 5'));
+// console.log(Number(' 4 5 '));
+// console.log(Number('     4     '));
+// console.log(Number('4n'));
+// console.log(Number('-4'));
+// console.log(Number('+4'));
+// console.log(Number('4*5'));
+
+// console.log(Number(true));
+// console.log(Number(false));
+
+// console.log(Number(null));
+// console.log(Number(undefined));
+// console.log(Number({ name: 'John', age: 27 }));
+
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(-10));
+// console.log(Boolean(Infinity));
+// console.log(Boolean(-Infinity));
+// console.log(Boolean(3n));
+// console.log(Boolean(0n));
+// console.log(Boolean(-5n));
+
+// console.log(Boolean('yes'));
+// console.log(Boolean('no'));
+// console.log(Boolean(' '));
+// console.log(Boolean(''));
+
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean('null'));
+// console.log(Boolean('undefined'));
+
+// console.log(Boolean({ name: 'John', age: 27 }));
+// console.log(Boolean({}));
+
+// console.log(BigInt(14));
+// console.log(BigInt(0));
+// console.log(BigInt(-13));
+// console.log(BigInt(Infinity));
+// console.log(BigInt(NaN));
+// console.log(BigInt(null));
+// console.log(BigInt(undefined));
+// console.log(BigInt('45'));
+// console.log(BigInt('+45'));
+// console.log(BigInt('-45'));
+// console.log(BigInt('4.5'));
+// console.log(BigInt('4b'));
+// console.log(BigInt(true));
+// console.log(BigInt(false));
+console.log(BigInt(''));
+console.log(BigInt(' '));
+console.log(Number(''));
+console.log(Number(' '));
