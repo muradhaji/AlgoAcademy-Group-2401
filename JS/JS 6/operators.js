da6bb0411d49daef6b8ac7366dc365de4a48c1ec
muradhaji <muradhajiyev42@icloud.com>
2024-06-21 15:39:53 +0400

    Operators explained

---
+++ JS/JS 6/operators.js
@@ -0,0 +1,99 @@
+// console.log(true & true);
+// console.log(false & false);
+// console.log(true & false);
+// console.log(false & true);
+
+// console.log(true | true);
+// console.log(false | false);
+// console.log(true | false);
+// console.log(false | true);
+
+// console.log(true ^ true);
+// console.log(false ^ false);
+// console.log(true ^ false);
+// console.log(false ^ true);
+
+// console.log(!true);
+// console.log(!false);
+
+// console.log(5 & 2);
+// 101
+// &
+// 010
+// =
+// 000
+// console.log(5 | 2);
+// 101
+// |
+// 010
+// =
+// 111
+// console.log(5 ^ 2);
+// 101
+// ^
+// 010
+// =
+// 111
+
+// console.log(10 & 12)
+// console.log(10 | 12)
+// console.log(10 ^ 12)
+
+// 1010
+// 1100
+
+// 1000 - 8
+// 1110 - 14
+// 0110 - 6
+
+// console.log(~7);
+// console.log(~14);
+// console.log(~-13);
+
+// console.log(3 << 4);
+// console.log(48 >> 1);
+// console.log(47 >> 2);
+// console.log(-5 >> 1);
+
+// -5 = 11111111111111111111111111111011
+// -5 = 01111111111111111111111111111101
+
+// 00000010101
+// >> 00000001010
+// << 00000101010
+
+// 000000001101
+// 111111110010
+// >> 111111111001
+// << 111111100100
+// >>> 011111111001
+
+// let a;
+// let b;
+// let c;
+// let d = ((a = 2), (b = 3), (c = a * b), b - a);
+
+// let a1 = 5;
+// let a2 = 7;
+// let a3 = (a1, a2);
+
+// console.log(5 > 6);
+// console.log(5 >= 6);
+// console.log(5 < 6);
+// console.log(5 <= 6);
+// console.log(5 == 6);
+// console.log(5 != 6);
+// console.log('6' == 6);
+// console.log('6' === 6);
+// console.log('6' != 6);
+// console.log('6' !== 6);
+// console.log('5' !== 6);
+// console.log(5 !== 6);
+// console.log(6 !== 6);
+// console.log('6' == '6');
+// console.log('6' != '6');
+// console.log('6' === '6');
+// console.log('6' !== '6');
+
+console.log('Asd' == 'asd');
+console.log('Asd' == 'Asd');
