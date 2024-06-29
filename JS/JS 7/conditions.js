6354001b579c2a76099fd8bb835c84986b95ba8b
muradhaji <muradhajiyev42@icloud.com>
2024-06-29 13:11:01 +0400

    Conditions and Arrays explained

---
+++ JS/JS 7/conditions.js
@@ -0,0 +1,172 @@
+// if(condition) {
+//    Correct
+// } else {
+//    Wrong
+// }
+
+// let a = 5;
+// let b = 4;
+
+// if (a < b) {
+// if (true) {
+// if (false) {
+// if (1) {
+// if (0) {
+// if ("asd") {
+// if ("") {
+// if (null) {
+// if (undefined) {
+// if (0n) {
+// if (13n) {
+// if (a > b) {
+// if (a == b) {
+// if (a != b) {
+// if (a === b) {
+
+// if (a !== b) {
+//   console.log('A less than B');
+// } else {
+//   console.log('A not less than B');
+// }
+
+// let a = Number(prompt('Enter A:')); // 3
+// let b = Number(prompt('Enter B:')); // 3
+
+// if (a < b) {
+//   console.log('a less than b');
+// } else {
+//   if (a > b) {
+//     console.log('a bigger than b');
+//   } else {
+//     console.log('a equals b');
+//   }
+// }
+
+// let x = Number(prompt('Enter number!'));
+
+// if (x > 0 && x < 100) {
+//   console.log('x is negative');
+// }
+
+// if (x < 0) {
+//   if (x < -100) {
+//     console.log('x is less than -100');
+//   } else {
+//     if (x > -100) {
+//       console.log('X between -100 and 0');
+//     } else {
+//       console.log('X equals -100');
+//     }
+//   }
+// } else {
+//   if (x > 0) {
+//     if (x > 100) {
+//       console.log('X is greater than 100');
+//     } else {
+//       if (x < 100) {
+//         console.log('X between 0 and 100');
+//       } else {
+//         console.log('X equals 100');
+//       }
+//     }
+//   } else {
+//     console.log('X equals 0');
+//   }
+// }
+
+// if (condition) {}
+// else if (condition) {}
+// else {}
+
+// if (x < -100) {
+//   console.log("< -100");
+// }
+// else if (x == -100) {
+//   console.log("== -100");
+// }
+// else if (x > -100 && x < 0) {
+//   console.log("> -100 and < 0");
+// }
+// else if (x == 0) {
+//   console.log("== 0");
+// }
+// else if (x > 0 && x < 100) {
+//   console.log("> 0 and < 100");
+// }
+// else if (x == 100) {
+//   console.log("== 100");
+// }
+// else {
+//   console.log("> 100");
+// }
+
+// let a = Number(prompt('Enter A:'));
+// let b = Number(prompt('Enter B:'));
+// let c = Number(prompt('Enter C:'));
+// let max;
+// let min;
+
+// if (a > b && a > c) {
+//   max = a;
+// } else if (b > a && b > c) {
+//   max = b;
+// } else {
+//   max = c;
+// }
+
+// if (a < b && a < c) {
+//   min = a;
+// } else if (b < a && b < c) {
+//   min = b;
+// } else {
+//   min = c;
+// }
+
+// console.log('Max:', max);
+// console.log('Min:', min);
+
+// console.log(condition ? correct : wrong);
+// console.log(4 > 5 ? 'Less' : 'Great');
+
+// let a = Number(prompt('Enter A:'));
+// let b = Number(prompt('Enter B:'));
+// let max;
+
+// if (a > b) {
+//   max = a;
+// } else {
+//   max = b;
+// }
+// console.log(max);
+
+// max = a > b ? a : b;
+
+// a < b ? console.log(b) : console.log(a);
+
+// let a = Number(prompt('Enter A:'));
+// let b = Number(prompt('Enter B:'));
+// let c = Number(prompt('Enter C:'));
+// let max;
+
+// if (a > b) {
+//   if (a > c) {
+//     max = a;
+//   } else {
+//     max = c;
+//   }
+// } else {
+//   if (b > c) {
+//     max = b;
+//   } else {
+//     max = c;
+//   }
+// }
+
+// max = a > b ? (a > c ? a : c) : (b > c ? b : c);
+// a > b
+//   ? a > c
+//     ? console.log(a)
+//     : console.log(c)
+//   : b > c
+//   ? console.log(b)
+//   : console.log(c);
---
+++ JS/JS 8/arrays.js
@@ -0,0 +1,52 @@
+// let user1 = 'John';
+// let user2 = 'Mike';
+// let user3 = 'Elliot';
+
+let user = {
+  name: 'John',
+  age: 27,
+  isMarried: false,
+  0: 'Value',
+};
+// {key:value, key: value}
+
+// console.log(user);
+// console.log(user.name);
+// console.log(user['name']);
+// console.log(user.age);
+// console.log(user['age']);
+// console.log(user.isMarried);
+// console.log(user['isMarried']);
+// console.log(user.height);
+// console.log(user.name, user.age, user.isMarried);
+
+// let users = ['John', 'Mike', 'Elliot', "Michael"];
+// [value, value, value]
+// [0, 1, 2]
+
+// console.log(user);
+// console.log(users);
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+
+// let arr = ['John', true, null, 45n, false, 7, undefined];
+// console.log(arr);
+// console.log(arr["0"]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr.length);
+
+// arr.length;
+// arr.pop();
+// arr.push();
+// arr.shift();
+// arr.unshift();
+// arr.includes();
+// arr.concat();
+// arr.indexOf();
+// arr.join();
+// arr.reverse();
+// arr.slice();
+// arr.sort();
+// arr.toString();
